13015b963628f1668648876e8af0486fcbef9b2c
vpashnya <vpashnya@gmail.com>
2025-02-02 20:50:05 +0300

    Занятие 13 Аннотации Исправление замечаний

--- src/main/java/ru/levelup/lesson13/proxyclasses/AccountProxyHandler.java
+++ src/main/java/ru/levelup/lesson13/proxyclasses/AccountProxyHandler.java
@@ -1,25 +1,22 @@
 package ru.levelup.lesson13.proxyclasses;
 
+import lombok.AllArgsConstructor;
 import ru.levelup.lesson12.accounts.Account;
 import ru.levelup.lesson13.annotations.Blocked;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.stream.Stream;
 
+@AllArgsConstructor
 public class AccountProxyHandler implements InvocationHandler {
-    Account account;
-
-    public AccountProxyHandler(Account account) {
-        this.account = account;
-    }
+    private Account account;
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if(Stream.of(method.getAnnotations()).map(a->a.annotationType()).toList().contains(Blocked.class)){
+        if (method.isAnnotationPresent(Blocked.class)) {
             System.out.println("Method %s is blocked".formatted(method.getName()));
             return null;
-        }else{
+        } else {
             return method.invoke(account, args);
         }
     }
